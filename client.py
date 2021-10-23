706d62c40415809c1e1946b4ac396f90e98ee430
Blikaj <46620120+Blikaj@users.noreply.github.com>
2021-10-23 18:18:31 +0300

    Updated client.py

--- client.py
+++ client.py
@@ -1,17 +1,23 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import socket
-
+hostname = (input('Enter hostname: '))
+sket = (input('Enter adress: '))
+if (len(hostname) == 0):
+	hostname = 'localhost'
+if (len(sket) == 0):
+	sket = "8080"
 sock = socket.socket()
-sock.connect(('localhost', 8080))
-senddat = input('Enter the message: ')
-senddat = senddat.encode('utf-8')
-sock.send(senddat)
+sock.connect((hostname, int(sket)))
+while True:
+	senddat = input('Enter the message: ')
+	senddat = senddat.encode('utf-8')
+	sock.send(senddat)
 
-data = sock.recv(1024)
-data = data.decode('utf-8')
+	data = sock.recv(1024)
+	data = data.decode('utf-8')
+	print (data)
+	if data == "EXIT":
+		break
 sock.close()
-
-print (data)
