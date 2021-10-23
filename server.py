f7cdd16889abd06036be0220d365c5cbf01acf9d
Blikaj <46620120+Blikaj@users.noreply.github.com>
2021-10-23 18:19:07 +0300

    Updated server.py

--- server.py
+++ server.py
@@ -3,17 +3,27 @@
 
 import socket
 
+def datasender(conn):
+    while True:
+    	data = conn.recv(1024)
+    	datacheck = (data.decode("utf-8"))
+    	print (datacheck)
+    	if datacheck.upper == 'EXIT':
+    		conn.send(data.upper())
+    		break
+    	else:
+    		if not data:
+    			break
+    		else:
+    			conn.send(data.upper())
+
 sock = socket.socket()
 sock.bind(('', 8080))
-sock.listen(1)
-conn, addr = sock.accept()
-
-print ('connected:', addr)
-
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
+	sock.listen(1)
+	conn, addr = sock.accept()
+
+	print ('connected:', addr)
+	datasender(conn)
 
 conn.close()
